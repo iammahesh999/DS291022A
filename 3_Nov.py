0f2ecf7103f36b5b39cef38a8a89a0a2a6eda096
Uttam Grade <47376167+Uttam-Grade-McK@users.noreply.github.com>
2022-11-03 21:28:44 +0530

    Data Types

---
+++ 3_Nov.py
@@ -0,0 +1,30 @@
+# Python is Interprated language ( line by line)
+# Python has easy language to learn because for easy syntaxes
+# Python has multiple frameworks(IDE's) vscode, jupyter, etc.
+
+#--------------------------------------------------
+####################### Data Types (Types of Data) #####################
+# 2,3 20 (any number) --> integer ( numbers) --> int
+# 2.3, 3.4, 5.63 ( ANy number) --> float ( decimal) --> float
+# uttam, abcd, machine learning --> string ( characters) --> str
+# True, False --> Boolean ( boolean) --> bool
+# a + ib ---> complex ( complex numbers) --> complex
+
+### Print() ### ( inbuild ( someone has already build for us))
+print(12)
+print(12.4)
+print(True)
+print('uttam')
+print("uttam")
+print('true') ## if we put something into the '' or "" it wiil be converted into the string always.
+print("True")
+
+print(type(12))
+print(type('12'))
+print(type('True'))
+print(type(True))
+print(type(46.5))
+
+
+
+
